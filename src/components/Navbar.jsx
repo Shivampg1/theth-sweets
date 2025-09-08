b1f9185a4144d571a10e7d46012b1d6006b4cca5
Shivampg1 <147384496+Shivampg1@users.noreply.github.com>
2025-09-08 23:38:27 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,14 +4,19 @@ import { Link } from "react-router-dom";
 export default function Navbar({ cartCount }) {
   return (
     <nav className="flex justify-between items-center bg-orange-600 text-white px-6 py-4 shadow-md">
+      {/* Brand/Logo */}
       <Link to="/" className="text-xl font-bold hover:text-yellow-200">
         🍬 The Thath Bihari
       </Link>
 
-      {/* Links with wide spacing */}
-      <div className="flex space-x-20 text-lg">  
-        <Link to="/" className="hover:text-yellow-200">Home</Link>
-        <Link to="/menu" className="hover:text-yellow-200">Menu</Link>
+      {/* Navigation Links */}
+      <div className="flex gap-x-8 text-lg">
+        <Link to="/" className="hover:text-yellow-200">
+          Home
+        </Link>
+        <Link to="/menu" className="hover:text-yellow-200">
+          Menu
+        </Link>
         <Link to="/cart" className="hover:text-yellow-200">
           Cart ({cartCount})
         </Link>
