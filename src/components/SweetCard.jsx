fcf7aa8ea185be6d7f7cbb225dc801828dbe7bb3
Shivampg1 <147384496+Shivampg1@users.noreply.github.com>
2025-09-10 13:58:08 +0530

    Update SweetCard.jsx

--- src/components/SweetCard.jsx
+++ src/components/SweetCard.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function SweetCard({ sweet, addToCart }) {
+export default function SweetCard({ sweet, onAdd }) {
   return (
     <div className="p-4 border rounded-2xl shadow-lg bg-white hover:shadow-2xl transform hover:scale-105 transition duration-300 text-center">
       <img 
@@ -9,9 +9,9 @@ export default function SweetCard({ sweet, addToCart }) {
         className="w-full h-48 object-cover rounded-lg"
       />
       <h2 className="text-2xl font-semibold mt-3">{sweet.name}</h2>
-      <p className="text-gray-600 text-lg">₹{sweet.price}</p>
+      <p className="text-gray-600 text-lg">{sweet.price}</p>
       <button
-        onClick={addToCart}   {/* ✅ just call it directly */}
+        onClick={onAdd}
         className="bg-orange-500 hover:bg-orange-600 text-white px-6 py-2 mt-3 rounded-lg transition"
       >
         Add to Cart
