3546c4ef80ecdfba0801d6e3016e947927e58f37
Shivampg1 <147384496+Shivampg1@users.noreply.github.com>
2025-09-14 19:35:57 +0530

    Update Menu.jsx

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -2,14 +2,15 @@ import React from "react";
 import SweetCard from "../components/SweetCard";
 
 const sweets = [
-  { name: "Thekua", price: "399/packet", priceValue: 399, img: "/images/thekua.jpg" },
-  { name: "Suji Halwa", price: "60/plate", priceValue: 60, img: "/images/suji.jpg" },
-  { name: "Gujiya", price: "20/pcs", priceValue: 20, img: "/images/gujiya.jpg" },
-  { name: "Ananas", price: "160/kg", priceValue: 160, img: "/images/methi.jpg" },
-  { name: "Malpua", price: "40/pcs", priceValue: 40, img: "/images/malpua.jpg" },
-  { name: "Khoa Mix Cake", price: "180/kg", priceValue: 180, img: "/images/khoa.jpg" },
-  { name: "Haldi Halwa", price: "80/plate", priceValue: 80, img: "/images/haldi.jpg" }
+  { name: "Thekua", price: "₹399/packet", priceValue: 399, img: "/images/thekua.jpg" },
+  { name: "Suji Halwa", price: "₹60/plate", priceValue: 60, img: "/images/suji.jpg" },
+  { name: "Gujiya", price: "₹20/pcs", priceValue: 20, img: "/images/gujiya.jpg" },
+  { name: "Ananas", price: "₹160/kg", priceValue: 160, img: "/images/methi.jpg" },
+  { name: "Malpua", price: "₹40/pcs", priceValue: 40, img: "/images/malpua.jpg" },
+  { name: "Khoa Mix Cake", price: "₹180/kg", priceValue: 180, img: "/images/khoa.jpg" },
+  { name: "Haldi Halwa", price: "₹80/plate", priceValue: 80, img: "/images/haldi.jpg" }
 ];
+
 
 export default function Menu({ addToCart }) {
   // Creates a DOM toast at top-center, removes any previous toast first
